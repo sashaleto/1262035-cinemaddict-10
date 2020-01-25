22e293c99f6cb6bbb5964b0722d77e53cebf6cd0
sashaleto <aleks.nesc@gmail.com>
2020-01-26 00:44:58 +0300

    Выделение статистики в футере в отдельный компонент

--- src/main.js
+++ src/main.js
@@ -2,20 +2,22 @@ import UserProfile from './components/user-profile';
 import NavigationComponent from './components/main-navigation';
 import MainFiltersComponent from './components/main-filters';
 import BoardComponent from './components/board';
+import FooterStatistics from "./components/footer-statistics";
+
+import PageController from "./controllers/page";
 
 import {getWatchedFilmsCount} from './utils';
 import {RenderPosition, render} from './utils/render';
 
 import {generateFilms} from "./moks/films";
 import {NAVIGATION} from "./moks/main-navigation";
-import PageController from "./controllers/page";
 
 const FILMS_COUNT = 23;
 
 const mainElement = document.querySelector(`.main`);
 const headerElement = document.querySelector(`.header`);
 const footerElement = document.querySelector(`.footer`);
-
+footerElement.querySelector(`.footer__statistics`).remove();
 
 const films = generateFilms(FILMS_COUNT);
 const userRating = getWatchedFilmsCount(films);
@@ -27,6 +29,7 @@ render(mainElement, new MainFiltersComponent(), RenderPosition.BEFOREEND);
 const boardComponent = new BoardComponent();
 render(mainElement, boardComponent, RenderPosition.BEFOREEND);
 
-renderBoard(boardComponent, films);
+const page = new PageController(boardComponent);
+page.render(films);
 
-footerElement.querySelector(`.footer__statistics`).innerHTML = `<p>${films.length} movies inside</p>`;
+render(footerElement, new FooterStatistics(films), RenderPosition.BEFOREEND);
