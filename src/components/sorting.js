26b663cf04dbe6e1191caecf0ae8a82ff512e860
sashaleto <aleks.nesc@gmail.com>
2020-01-26 13:51:01 +0300

    Реализация метода подписки на изменение типа сортиовки

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -17,7 +17,32 @@ const createSortingTemplate = () => {
 };
 
 export default class SortingComponent extends AbstractComponent {
+  constructor() {
+    super();
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortingTemplate();
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currentSortType === sortType) {
+        return;
+      }
+
+      this._currentSortType = sortType;
+
+      handler(this._currentSortType);
+    });
   }
 }
