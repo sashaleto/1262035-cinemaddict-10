204cf1a3acd70cb740c420455c5c864c5684ebd5
sashaleto <aleks.nesc@gmail.com>
2020-01-31 00:26:14 +0300

    Рефакторинг контроллера фильма, выделение приватных методов

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -7,34 +7,41 @@ export default class MovieController {
   constructor(container) {
     this._container = container;
     this._popupContainer = document.querySelector(`body`);
+
+    this._filmComponent = null;
+    this._filmPopupComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+    this._showPopup = this._showPopup.bind(this);
+    this._removePopup = this._removePopup.bind(this);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._removePopup();
+    }
+  }
+
+  _removePopup() {
+    remove(this._filmPopupComponent);
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+  }
+
+  _showPopup() {
+    render(this._popupContainer, this._filmPopupComponent, RenderPosition.BEFOREEND);
+    this._filmPopupComponent.setCloseButtonClickHandler(this._removePopup);
+    document.addEventListener(`keydown`, this._onEscKeyDown);
   }
 
   render(film) {
-    const filmComponent = new FilmCardComponent(film);
+    this._filmComponent = new FilmCardComponent(film);
     const filmComments = generateComments(4);
-    const filmPopupComponent = new FilmPopupComponent(film, filmComments);
+    this._filmPopupComponent = new FilmPopupComponent(film, filmComments);
 
-    render(this._container, filmComponent, RenderPosition.BEFOREEND);
+    render(this._container, this._filmComponent, RenderPosition.BEFOREEND);
 
-    const showPopup = () => {
-      render(this._popupContainer, filmPopupComponent, RenderPosition.BEFOREEND);
-      filmPopupComponent.setCloseButtonClickHandler(removePopup);
-      document.addEventListener(`keydown`, onEscKeyDown);
-    };
-
-    const removePopup = () => {
-      remove(filmPopupComponent);
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    };
-
-    const onEscKeyDown = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-      if (isEscKey) {
-        removePopup();
-      }
-    };
-
-    filmComponent.setOpenPopupListeners(showPopup);
+    this._filmComponent.setOpenPopupListeners(this._showPopup);
   }
 }
